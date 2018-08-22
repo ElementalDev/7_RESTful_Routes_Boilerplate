4eb67efdaf2846d1f697ee2e2dd68c98d9684bf1
Reece_Collett <rcollett@spartaglobal.com>
2018-08-22 21:07:42 +0100

    Added route 1 to RESTful Routes

--- RESTful_routes.rb
+++ RESTful_routes.rb
@@ -3,3 +3,11 @@ require "sinatra/contrib"
 
 # This will restart sinatra every time the IDE is saved
 require "sinatra/reloader" if development?
+
+# READ ROUTE
+
+# This will return all of the results of a page as there are no specific parameters
+# to determine what the server gets
+get "/" do
+  "This will return a string"
+end
